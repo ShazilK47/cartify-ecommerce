f0004f43ffc3939adf18349c0734ac93041cb7ee
ShazilK47 <shazil.akn@gmail.com>
2025-01-31 19:16:16 +0500

    adding sale functionality

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,14 +1,25 @@
+import BlackFridayBanner from "@/components/BlackFridayBanner";
+import ProductView from "@/components/ProductView";
 import { Button } from "@/components/ui/button";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
 export default async function Home() {
   const products = await getAllProducts();
+  const categories = await getAllCategories();
+
+  // console.log(
+  //   crypto.randomUUID().slice(0, 5) +
+  //     `>>> Rendered the home page cache with ${products.length} products and ${categories.length} categories`
+  // );
 
   return (
     <div>
-      <h1>Hello World </h1>
+      <BlackFridayBanner />
       {/* render all the products */}
-      <Button>click me</Button>
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductView products={products} categories={categories} />
+      </div>
     </div>
   );
 }
--- sanity/lib/products/getAllProducts.ts
+++ sanity/lib/products/getAllProducts.ts
@@ -1,4 +1,22 @@
 import { defineQuery } from "next-sanity";
 import { sanityFetch } from "../live";
 
-export const getAllProducts = async () => {};
+export const getAllProducts = async () => {
+  const ALL_PRODUCTS_QUERY = defineQuery(`
+            *[
+                _type == "product"
+            ] | order(name asc)
+        `);
+
+  try {
+    // use sanity fetch to send the query
+    const products = await sanityFetch({
+      query: ALL_PRODUCTS_QUERY,
+    });
+
+    return products.data || [];
+  } catch (error) {
+    console.error("Error fetching all products:", error);
+    return []; // Return an empty array on error
+  }
+};
